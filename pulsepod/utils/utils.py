f318882d1a4c2a4c7621169048d7db6fe9f32d5d
Kelly Caylor <kcaylor@princeton.edu>
2014-03-27 15:06:11 -0400

    debugging sensor API calls

--- pulsepod/utils/utils.py
+++ pulsepod/utils/utils.py
@@ -53,6 +53,7 @@ def get_sensor(sid):
 		raise InvalidMessage('sid not found',status_code=400)
 			
 	try:
+		print s.json()
 		sensor['value_length'] = resp[u'nbytes']
 		sensor['name'] = resp[u'name'] 
 		sensor['_id'] = resp[u'_id']
